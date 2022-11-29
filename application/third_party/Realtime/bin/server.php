3ddd086b3c489e6978a2593a0b1c13541ae02dfe
Kabdeuttkarsh <35558249+Kabdeuttkarsh@users.noreply.github.com>
2022-11-29 16:46:32 +0530

    h

--- application/third_party/Realtime/bin/server.php
+++ application/third_party/Realtime/bin/server.php
@@ -8,7 +8,7 @@ use Ratchet\Server\IoServer;
 use Ratchet\Http\HttpServer;
 use Ratchet\WebSocket\WsServer;
 
-$server = IoServer::factory(new HttpServer(new WsServer(new Chat)), 2000);
+$server = IoServer::factory(new HttpServer(new WsServer(new Chat)), 8080);
 
 $server->run();
 
