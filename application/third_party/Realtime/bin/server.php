9547e984b719ed382a595bac244b0b1b5daa14c1
Kabdeuttkarsh <35558249+Kabdeuttkarsh@users.noreply.github.com>
2022-12-15 15:40:27 +0530

    h

--- application/third_party/Realtime/bin/server.php
+++ application/third_party/Realtime/bin/server.php
@@ -13,3 +13,79 @@ $server = IoServer::factory(new HttpServer(new WsServer(new Chat)), 2000);
 $server->run();
 
 ?>
+
+<script type="text/javascript">
+var conn = new Connection2(Broadcast.BROADCAST_URL+":"+Broadcast.BROADCAST_PORT);
+
+
+var typeData = {broadType : Broadcast.POST, data:"Hii"};
+conn.sendMsg(typeData); 
+
+function Connection2(url) {
+
+
+      this.open = false;
+
+      this.socket = new WebSocket("wss://" + url);
+      
+      this.setupConnectionEvents();
+
+    }
+
+  var Connection2 = (function(){
+
+  function Connection2(url) {
+
+
+      this.open = false;
+
+      this.socket = new WebSocket("wss://" + url);
+      
+      this.setupConnectionEvents();
+
+    }
+
+  Connection2.prototype = {
+    
+    setupConnectionEvents : function () {
+          var self = this;
+
+          self.socket.onopen = function(evt) { self.connectionOpen(evt); };
+          self.socket.onmessage = function(evt) { self.connectionMessage(evt); };
+          self.socket.onclose = function(evt) { self.connectionClose(evt); };
+      },
+
+      connectionOpen : function(evt){
+          this.open = true;
+          this.addSystemMessage("Connected");
+          console.log("connection Established");
+    },
+      connectionMessage : function(evt){
+          var data = JSON.parse(evt.data);
+         //   console.log(data.msg);
+          this.addChatMessage(data.msg);
+        console.log("Message Sent");
+      },
+      connectionClose : function(evt){
+          this.open = false;
+          this.addSystemMessage("Disconnected");
+           console.log("Disconnected");
+      },
+
+      sendMsg : function(message){
+        
+          this.socket.send(JSON.stringify({
+              msg : message
+          }));
+
+     
+      },
+
+    };
+
+    return Connection2;
+
+})();
+
+</script>
+
