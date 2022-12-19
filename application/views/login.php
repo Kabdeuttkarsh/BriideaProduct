f4af24ee182dbdddbc504f473969978e71e0e96a
Kabdeuttkarsh <35558249+Kabdeuttkarsh@users.noreply.github.com>
2022-12-19 16:53:29 +0530

    sw

--- application/views/login.php
+++ application/views/login.php
@@ -177,6 +177,33 @@ else{
 
 
 <script type="text/javascript">
+  const registerServiceWorker = async () => {
+  if ("serviceWorker" in navigator) {
+    try {
+      const registration = await navigator.serviceWorker.register("/sw.js", {
+        scope: "/",
+      });
+      if (registration.installing) {
+        console.log("Service worker installing");
+      } else if (registration.waiting) {
+        console.log("Service worker installed");
+      } else if (registration.active) {
+        console.log("Service worker active");
+      }
+    } catch (error) {
+      console.error(`Registration failed with ${error}`);
+    }
+  }
+};
+
+// …
+
+registerServiceWorker();
+
+</script>
+
+
+<script type="text/javascript">
   $('#LoginForm').on('click', '.item-forgetPassword', function(){
       $('#ForgetPasswordModal').modal('show');
 
